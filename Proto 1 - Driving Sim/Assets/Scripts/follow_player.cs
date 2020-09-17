66b9bd20e260ac3066633de6f6d92d599a553165
Josh Hansen <joshua.h538@stu.nebo.edu>
2020-09-17 12:41:17 -0600

    Fixed gitignore and have added the Unity project
    message

---
+++ Proto 1 - Driving Sim/Assets/Scripts/follow_player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow_player : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset = new Vector3(0, 3, -7);
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //Makes the camera match the player's movement / position
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Proto 1 - Driving Sim/Assets/Scripts/player_controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    // Access Modifier Type Name Operator Value
+    private int speed = 15;
+    private float turnSpeed;
+    
+    public float horizontalInput;
+    public float forwardInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        //move the vehicle forward based on forwardInput
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //rotates vehicle left and right based on horizontalInput
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
